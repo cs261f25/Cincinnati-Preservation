071660d259aa80882cbe17b598857e5e965d243b
piiines <princejoshuabagcat@gmail.com>
2025-10-09 15:27:12 -0400

    Modify Filebrowser.java to remove file filter and Display button

--- slate/src/main/java/com/cincypreservation/FileBrowser.java
+++ slate/src/main/java/com/cincypreservation/FileBrowser.java
@@ -14,18 +14,15 @@ class filebrowser extends JFrame implements ActionListener {
     }
     public static void main(String args[])
     {
-        JFrame f = new JFrame("File Chooser - Display Photos");
+        JFrame f = new JFrame("File Chooser");
         f.setSize(400,400);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton button1 = new JButton("Display");
-        JButton button2 = new JButton("Open");
+        JButton button1 = new JButton("Browse Files");
         filebrowser f1 = new filebrowser();
         button1.addActionListener(f1);
-        button2.addActionListener(f1);
         JPanel p = new JPanel();
         p.add(button1);
-        p.add(button2);
         l = new JLabel("no file selected");
         p.add(l);
         f.add(p);
@@ -33,41 +30,18 @@ class filebrowser extends JFrame implements ActionListener {
     }
     public void actionPerformed(ActionEvent evt)
     {
-        
+        // Captures actions in the GUI.
         String com = evt.getActionCommand();
-        FileFilter filter = new FileNameExtensionFilter("Picture File",  new String[]{"jpg","jpeg","png"});
-        if (com.equals("Display")){ 
-            if(!testImages(l)){
-                l.setText("Image failed to load - file path might be invalid");
-            }
-        } 
-        else {
-            // Open Button
+        Filter filter = new Filter("xls");
+        if (com.equals("Browse Files")){ 
             JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            j.setFileFilter(filter);
-            j.addChoosableFileFilter(filter);
             int r = j.showOpenDialog(null);
             if (r == JFileChooser.APPROVE_OPTION){
                 l.setText(j.getSelectedFile().getAbsolutePath());
             }
             else {
-                l.setText("The user cancelled the operation");
+                l.setText("The user cancelled the operation.");
             }
         }   
-    }
-    private boolean testImages(JLabel l){
-        final String IMG_PATH = l.getText();
-        if (IMG_PATH.contains(".jpg")){
-            try{
-                BufferedImage img = ImageIO.read(new File(IMG_PATH));
-                ImageIcon icon = new ImageIcon(img);
-                JLabel photolabel = new JLabel(icon);
-                JOptionPane.showMessageDialog(null, photolabel);
-                return true;
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-        return false;
     }
 }

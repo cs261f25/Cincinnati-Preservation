df8e7c280fb90c01f876f3c6a8a1df62ba179acb
piiines <princejoshuabagcat@gmail.com>
2025-10-09 14:58:57 -0400

    Modified FileBrowser.java for use with Excel Files

--- slate/src/main/java/com/cincypreservation/FileBrowser.java
+++ slate/src/main/java/com/cincypreservation/FileBrowser.java
@@ -1,10 +1,6 @@
 package com.cincypreservation;
-import java.io.*;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-
 import javax.swing.filechooser.*;
 import javax.swing.filechooser.FileFilter;
 class filebrowser extends JFrame implements ActionListener {
@@ -14,35 +10,25 @@ class filebrowser extends JFrame implements ActionListener {
     }
     public static void main(String args[])
     {
-        JFrame f = new JFrame("File Chooser - Display Photos");
+        JFrame f = new JFrame("File Chooser - Open Excel File");
         f.setSize(400,400);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton button1 = new JButton("Display");
-        JButton button2 = new JButton("Open");
+        JButton button1 = new JButton("Open File");
         filebrowser f1 = new filebrowser();
         button1.addActionListener(f1);
-        button2.addActionListener(f1);
         JPanel p = new JPanel();
         p.add(button1);
-        p.add(button2);
-        l = new JLabel("no file selected");
+        l = new JLabel("No file selected.");
         p.add(l);
         f.add(p);
         f.setVisible(true);
     }
     public void actionPerformed(ActionEvent evt)
     {
-        
         String com = evt.getActionCommand();
-        FileFilter filter = new FileNameExtensionFilter("Picture File",  new String[]{"jpg","jpeg","png"});
-        if (com.equals("Display")){ 
-            if(!testImages(l)){
-                l.setText("Image failed to load - file path might be invalid");
-            }
-        } 
-        else {
-            // Open Button
+        FileFilter filter = new FileNameExtensionFilter("Excel File",  new String[]{"xlsx","xls"});
+        if (com.equals("Open File")){ 
             JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
             j.setFileFilter(filter);
             j.addChoosableFileFilter(filter);
@@ -54,20 +40,5 @@ class filebrowser extends JFrame implements ActionListener {
                 l.setText("The user cancelled the operation");
             }
         }   
-    }
-    private boolean testImages(JLabel l){
-        final String IMG_PATH = l.getText();
-        if (IMG_PATH.contains(".jpg")){
-            try{
-                BufferedImage img = ImageIO.read(new File(IMG_PATH));
-                ImageIcon icon = new ImageIcon(img);
-                JLabel photolabel = new JLabel(icon);
-                JOptionPane.showMessageDialog(null, photolabel);
-                return true;
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-        return false;
     }
 }

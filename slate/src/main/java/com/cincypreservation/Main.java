c16434e0328145fdc32c9a608a242eef75e605a9
Nick Clarkson <clarksonn@xavier.edu>
2025-10-14 19:57:47 -0500

    added a main menu that allows the user to access functionality from the home page

--- slate/src/main/java/com/cincypreservation/Main.java
+++ slate/src/main/java/com/cincypreservation/Main.java
@@ -1,7 +1,50 @@
 package com.cincypreservation;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JToolBar;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Inspection Tracker App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,400);
+        JToolBar tb = new JToolBar("Functions");
+        
+        JButton cameraButton = new JButton("Open Camera");
+        JButton formButton = new JButton("Create new Form");
+        JButton internetButton = new JButton("Test Internet");
+        JButton fileButton = new JButton("Open File");
+
+        tb.add(cameraButton);
+        tb.add(formButton);
+        tb.add(internetButton);
+        tb.add(fileButton);
+        
+        frame.add(tb, BorderLayout.NORTH);
+        frame.pack();
+        frame.setVisible(true);
+
+        cameraButton.addActionListener(e -> {
+            LaunchCamera lc = new LaunchCamera();
+            lc.main(new String[1]);
+        });
+
+        formButton.addActionListener(e -> {
+            Form f = new Form();
+            f.main(new String[1]);
+        });
+
+        internetButton.addActionListener(e -> {
+            InternetTester it = new InternetTester();
+            it.main(new String[1]);
+        });
+
+        fileButton.addActionListener(e -> {
+            FileBrowser fb = new FileBrowser();
+            fb.main(new String[1]);
+        });
     }
 }

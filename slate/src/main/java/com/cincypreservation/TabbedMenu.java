23bc5850138921dbcb044da500fc9f0f62519781
piiines <princejoshuabagcat@gmail.com>
2025-11-28 11:41:47 -0500

    Separate Feature adding to a separate tab

--- slate/src/main/java/com/cincypreservation/TabbedMenu.java
+++ slate/src/main/java/com/cincypreservation/TabbedMenu.java
@@ -10,21 +10,26 @@ public class TabbedMenu extends JPanel {
         setLayout(new GridLayout(1,1));
         JTabbedPane tabbedPane = new JTabbedPane();
 
-        JComponent panel1 = new FormPanel();
-        tabbedPane.addTab("Tab 1 - File Writer ", panel1);
+        FormPanel formPanel = new FormPanel();
+        JComponent panel1 = formPanel;
+        tabbedPane.addTab("File Writer", panel1);
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
-        JComponent panel2 = new InternetTester();
-        tabbedPane.addTab("Tab 2 - Internet Tester", panel2);
+        JComponent panel2 = formPanel.new FeaturePanel();
+        tabbedPane.addTab("Feature Adder", panel2);
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
 
         JComponent panel3 = new PropertySelector();
-        tabbedPane.addTab("Tab 3 - Property Selector", panel3);
+        tabbedPane.addTab("Property Selector", panel3);
         tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
 
-        JComponent panel4 = new ButtonPanel();
-        tabbedPane.addTab("Tab 4 - Buttons", panel4);
+        JComponent panel4 = new InternetTester();
+        tabbedPane.addTab("Internet Tester", panel4);
         tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
+
+        JComponent panel5 = new ButtonPanel();
+        tabbedPane.addTab("Buttons", panel5);
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_5);
 
         add(tabbedPane);
     } 
